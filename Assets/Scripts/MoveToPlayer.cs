d06e14259cd17919fb47e4d88b56263b7cbbdfad
McChristie-Aidan <55122417+McChristie-Aidan@users.noreply.github.com>
2019-10-16 14:39:30 -0500

    Adds Midterm project

---
+++ Assets/Scripts/MoveToPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToPlayer : MonoBehaviour
+{
+    GameObject Player = null;
+    Vector3 PlayerPosition;
+    public float speed = 30f;
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Player == null)
+        {
+            Player = GameObject.Find("Player");
+        }
+        PlayerPosition = Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3.MoveTowards(this.gameObject.transform.position, PlayerPosition, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/ShotManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotManager : MonoBehaviour
+{
+    [SerializeField] GameObject ShotPrefab;
+    public float shootDelay = .5f;
+    public float shootRate = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Shoot", shootDelay, shootRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shoot()
+    {
+        Instantiate(ShotPrefab, transform.position, gameObject.transform.rotation);
+    }
+}
